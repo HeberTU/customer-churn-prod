f9e85ba60f0f89317d980ad4e88bd56163107d1b
Heber Trujillo <heber.trj.urt@gmail.com>
2022-03-08 09:08:28 +0100

    Adding exception to handle not implemented model.

---
+++ core/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Exceptions module.
+
+Created on: 08/03/2022
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+class ModelNotImplementedError(Exception):
+    """Exception raised if a ML model is note implemented."""
+    pass
